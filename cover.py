7395edefdda39ad856ff90263193acf4a48e8ade
Reself <846074811@qq.com>
2021-06-16 07:15:48 +0800

    bulid the frame of cover

--- cover.py
+++ cover.py
@@ -10,6 +10,8 @@ class cover:
         # self.data -> [x,n,32,3]
         self.data = data_2d_std.transpose(0,1)
 
+        # implement code to change (x,y,z) to (x,z,y) for self.data
+
         # self.head -> [x,n,3]
         self.head = self.data[:,:,head_index,:]
 
@@ -21,13 +23,53 @@ class cover:
         # self.data -> [x,32*n,3]
         self.data = self.data.reshape(self.x, self.n*self.m,3)
 
+        # self.cover -> [x,32*n,1] 
+        # record the cover, 1 represent cover 
+        self.cover = torch.zeros(self.x,self.m*self.n,1)
+
 
     def generate_cover(self, index):
         """
         generate the covering tensor
         index -> list<int>
         self.data -> [x,n,32,3]
-        self.cover -> [x,m,2,3]
+        return -> [x,m,2,3]
         """
         return torch.stack([self.data[:,:,i,:] for i in index],2).reshape(self.x,self.n,int(len(index)/2),2,3).reshape(self.x,self.n*int(len(index)/2),2,3)
         
+
+    def judge_head(self):
+        """
+        for single data, judge for every frame
+        """
+        # implement code
+
+
+    def judge_body(self):
+        """
+        for single data, judge for every frame
+        """
+        # implement code
+
+
+    def judge_leg(self):
+        """
+        for single data, judge for every frame
+        """
+        # implement code
+
+
+    def judge_arm(self):
+        """
+        for single data, judge for every frame
+        """
+        # implement code
+    
+    
+    def main(self):
+        self.judge_head()
+        self.judge_body()
+        self.judge_leg()
+        self.judge_arm()
+        self.data[:,:,:,-1] = self.cover
+        return self.data
