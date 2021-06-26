b2d937d519927f522767a79a4deb163f0f56d27a
Reself <846074811@qq.com>
2021-06-26 18:42:52 +0800

    fill the function about get cross cases

--- cover.py
+++ cover.py
@@ -13,7 +13,51 @@ class cover():
         #device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
         #self.data = data_2d_std.to(device=device)
         self.data = data_2d_std 
-        self.get_cross(self.data)
+        self.cross_cases = self.get_cross_cases(self.data)
+        print(self.cross_cases)
+        print(len(self.cross_cases))
+
+
+    def get_cross_cases(self, data_2d_std):
+        '''
+        get the bonding in x-axis and then judge for the cross area
+        m * [frame, min, max, list(id)]
+        list[id] sorted in depth
+        '''
+        x_info = data_2d_std[:,:,:,0]
+        
+        # bound -> [x,n,2] + depth -> [x,n]
+        bound = torch.stack([torch.min(x_info,-1).values,torch.max(x_info,-1).values,torch.min(data_2d_std[:,:,:,2],-1).values],-1).transpose(0,1).numpy()
+
+        # cross area
+        # for loop for single frame
+
+        cross_cases = list()
+        for i in range(len(bound)):
+            x = bound[i]
+            # sort for all the point
+            v = np.sort(x[:,:2].flatten())
+            # initial the left bound list, id set and cross result list
+            left = v[0]
+            set, cross = list(), list()
+            # traversal for the sorted list as right bound
+            for right in v:
+                index = (x==right).nonzero()
+                id = int(index[0])
+                d = x[id,2]            
+                if (index[1] == 0):
+                    set.append(d)
+                else:
+                    if (len(set) > 1):
+                        set.sort()      
+                        cross.append([i,left,right,[int((j==x).nonzero()[0]) for j in set]])
+                    set.remove(d)
+                left = right
+            cross_cases.append(cross.copy())
+        return cross_cases
+
+    def get_cover_joint(self):
+        pass
 
 
     def convert(self, data):
@@ -48,47 +92,3 @@ class cover():
             data[22,:],
             data[31,:]
         ])
-
-
-    def loc2pose(self):
-        '''
-        convert the joints 3D location to pose info
-        '''
-        pass
-
-
-    def get_cross(self, data_2d_std):
-        '''
-        get the bonding in x-axis and then judge for the cross area
-        '''
-        x_info = data_2d_std[:,:,:,0]
-        
-        # bound -> [x,n,2]
-        bound = torch.stack([torch.min(x_info,-1).values,torch.max(x_info,-1).values],-1).transpose(0,1).numpy()
-
-        # depth -> [x,n]
-        depth = torch.min(data_2d_std[:,:,:,2],-1).values.transpose(0,1).numpy()
-
-        # cross area
-        # for loop for single frame
-        
-        for x in bound:
-            # sort for all the point
-            v = np.sort(x.flatten())
-            # initial the left bound list, id set and cross result list
-            left = v[0]
-            set, cross = list(), list()
-            for right in v:
-                index = (x==right).nonzero()
-                id = index[0]
-                if (index[1] == 0):
-                    set.append(id)
-                    continue
-                else:
-                    set.remove(id)
-                    cross.append([left,right,set])
-                    left = right
-            print(cross)
-            print('\n')
-        
-            
