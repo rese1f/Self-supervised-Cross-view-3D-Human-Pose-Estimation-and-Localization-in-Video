4ea061c37a5601868bd585dc6e088ca46a72d09a
Mu Xie <xiem@live.com>
2021-07-04 09:08:04 +0800

    add new features, changed data type

--- camera.py
+++ camera.py
@@ -1,15 +1,17 @@
 import numpy as np
-import torch
+#import torch
 from configparser import ConfigParser
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-torch.set_default_tensor_type(torch.DoubleTensor)
-
+#torch.set_default_tensor_type(torch.DoubleTensor)
+'''
+[Alert] this project have changed all the tensor of Pytorch into ndarray of Numpy!! Be sure to input proper datatype!
+'''
 
 class Camera:
 
-    def __init__(self, data=None, frames=10, tracking=1, type = "phone") -> None:
+    def __init__(self, data=None, frames=10, tracking=1, type = "phone", loadpath = None) -> None:
         """Initialization & Localization
 
         Initialization part with introduction to all the local variables.
@@ -30,19 +32,35 @@ class Camera:
                 statement.
         """
 
-        self.ifTracking = tracking
-        self.frame = frames
-        
-        try:
-            Camera.__get_center(self,data)
-        except:
-            print("data needed")
+            
+
+        if loadpath is not None:
+            Camera.__load_camera(self, loadpath)
+            if type == "phone":
+                Camera.phone(self,load = 1)
+            elif type == "monitor":
+                pass
+        else:
+
+            self.ifTracking = tracking
+            self.frame = frames
+
+            dataShape = data.size()
+            self.x = dataShape[0]
+            self.n = dataShape[1]
+            self.y = dataShape[2]
+
+            try:
+                Camera.__get_center(self,data)
+            except:
+                print("data needed")
             pass
 
-        if type == "phone":
-            Camera.phone(self,data)
-        elif type == "monitor":
-            pass
+            if type == "phone":
+                Camera.phone(self,data)
+            elif type == "monitor":
+                pass
+        
         #Camera.__visc(self)
 
         return
@@ -73,30 +91,28 @@ class Camera:
             posCamera = self.camera_pos[i]
             argCamera = self.camera_arg[i]
 
-            Rz = torch.tensor(np.array(
+            Rz = np.array(
                 [[np.cos(argCamera[2]), -np.sin(argCamera[2]), 0.], [np.sin(argCamera[2]), np.cos(argCamera[2]), 0.],
-                 [0., 0., 1.]], dtype=np.float16))
-            Ry = torch.tensor(np.array([[np.cos(argCamera[1]), 0., np.sin(argCamera[1])], [0., 1., 0.],
-                                        [-np.sin(argCamera[1]), 0., np.cos(argCamera[1])]], dtype=np.float16))
-            Rx = torch.tensor(np.array([[1., 0., 0.], [0., np.cos(argCamera[0]), -np.sin(argCamera[0])],
-                                        [0., np.sin(argCamera[0]), np.cos(argCamera[0])]], dtype=np.float16))
+                 [0., 0., 1.]], dtype=np.float16)
+            Ry = np.array([[np.cos(argCamera[1]), 0., np.sin(argCamera[1])], [0., 1., 0.],
+                                        [-np.sin(argCamera[1]), 0., np.cos(argCamera[1])]], dtype=np.float16)
+            Rx = np.array([[1., 0., 0.], [0., np.cos(argCamera[0]), -np.sin(argCamera[0])],
+                                        [0., np.sin(argCamera[0]), np.cos(argCamera[0])]], dtype=np.float16)
 
-            R = torch.mm(Rz, Ry)
-            R = torch.mm(R, Rx)
+            R = np.matmul(Rz, Ry)
+            R = np.matmul(R, Rx)
             R = R.transpose(0,1)
             self.R = R
 
-            T = - torch.mm(R, torch.tensor(np.array(posCamera, dtype=np.float16).reshape(3, 1)))
+            T = - np.matmul(R, np.array(posCamera, dtype=np.float16).reshape(3, 1))
 
-            H_o2k = torch.cat((R, T), 1)
-            H_o2k = torch.cat((H_o2k, torch.tensor([[0., 0., 0., 1.]])), 0)
+            H_o2k = np.concatenate((R, T), 1)
+            H_o2k = np.concatenate((H_o2k, np.array([[0., 0., 0., 1.]])), 0)
 
             if i == 0:
-                self.exmat = H_o2k
-            elif i == 1:
-                self.exmat = torch.stack((self.exmat, H_o2k))
+                self.exmat = H_o2k.reshape(1, 4, 4)
             else:
-                self.exmat = torch.cat((self.exmat, H_o2k.reshape(1, 4, 4)), 0)
+                self.exmat = np.concatenate((self.exmat, H_o2k.reshape(1, 4, 4)), 0)
 
         return
 
@@ -127,7 +143,7 @@ class Camera:
 
         # TODO: The distortions need to be added.
 
-        self.inmat = torch.tensor(np.array([[fx, s, u], [0, fy, v], [0, 0, 1]]))
+        self.inmat = np.array([[fx, s, u], [0, fy, v], [0, 0, 1]])
 
         return
 
@@ -138,14 +154,14 @@ class Camera:
         equivalent to 3d camera coordinate (homogeneous 2d coordinate).
 
         Args:
-            data_3d: the 3-D coordinates of the raw data, i.e. data_cluster, of size [n, x, 32, 3]
+            data_3d: the 3-D coordinates of the raw data, i.e. data_cluster, of size [n, x, 17, 3]
 
         Variables:
             n: the number of people
             x: the number of frames
 
         Returns:
-            datasT: of size [n, x, 32, 3]
+            datasT: of size [n, x, 17, 3]
 
         Raises:
             NOError: no error occurred up to now
@@ -156,17 +172,17 @@ class Camera:
         n = dataShape[1]
         y = dataShape[2]
 
-        datasInHC = torch.cat((data_3d, torch.tensor(np.ones((x, n, y, 1), dtype=np.float16))), dim=3)\
+        datasInHC = np.concatenate((data_3d, np.ones((x, n, y, 1), dtype=np.float16)), dim=3)\
             .reshape(x, n, y, 4, 1)
 
         # 交换person与frame维度
-        datasT = torch.transpose(datasInHC, 0, 1);
+        datasT = np.transpose(datasInHC, 0, 1);
 
         for i in range(self.frame):
-            datasT[i] = torch.matmul(self.exmat[i], datasT[i])
+            datasT[i] = np.matmul(self.exmat[i], datasT[i])
             i += 1
 
-        datasT = torch.transpose(datasT, 0, 1).reshape(x, n, y, 4)[:, :, :, :3]
+        datasT = np.transpose(datasT, 0, 1).reshape(x, n, y, 4)[:, :, :, :3]
         #datasT[:, :, :, 2] =  - datasT[:, :, :, 2]
 
         return datasT
@@ -178,21 +194,19 @@ class Camera:
         equivalent to pixel coordinate (2d to 2d).
 
         Args:
-            data: the 3-D coordinates of the raw data, i.e. data_cluster, data_3d, all of size [n, x, 32, 3]
+            data: the 3-D coordinates of the raw data, i.e. data_cluster, data_3d, all of size [n, x, 17, 3]
 
         Returns:
-            data: of the same size as input data, i.e. [n, x, 32, 3]
+            data: of the same size as input data, i.e. [n, x, 17, 3]
 
         Raises:
             NOError: no error occurred up to now
         """
-        #data = data.reshape([data.shape[0], data.shape[1], 32, 3, 1])
-        data = torch.unsqueeze(data,4)
-        data = torch.matmul(self.inmat, data)
-        data = data / torch.abs(torch.unsqueeze(data[:, :, :, 2], 3))
-        #data = data.reshape([data.shape[0], data.shape[1], 32, 3])\
-        data = torch.squeeze(data,-1)
-        
+        data = data.reshape([data.shape[0], data.shape[1], self.y, 3, 1])
+        data = np.matmul(self.inmat, data)
+        data = data / np.abs(np.unsqueeze(data[:, :, :, 2], 3))
+        data = data.reshape([data.shape[0], data.shape[1], self.y, 3])
+
         return data
 
     def __get_center(self, data):
@@ -202,18 +216,18 @@ class Camera:
         plane, so it has a z-value of 0.
 
         Args:
-            data: the 3-D coordinates of the raw data, i.e. data_cluster, data_3d, all of size [n, x, 32, 3]
+            data: the 3-D coordinates of the raw data, i.e. data_cluster, data_3d, all of size [n, x, 17, 3]
 
         Returns:
-            datas: of the same size as input data, i.e. [n, x, 32, 3]
+            datas: of the same size as input data, i.e. [n, x, 17, 3]
 
         Raises:
             NOError: no error occurred up to now
         """
         slice1 = data[:, 0, 10, 0]
         slice2 = data[:, 0, 10, 1]
-        sum1 = torch.sum(slice1)
-        sum2 = torch.sum(slice2)
+        sum1 = np.sum(slice1)
+        sum2 = np.sum(slice2)
 
         self.center = np.array([sum1, sum2, 0]) / 3
         print(self.center)
@@ -221,68 +235,100 @@ class Camera:
 
     def get_angle(frame, data):
         """
-        Get the angle
+        Get the camera euclidean angle from given directional vector
+
+        Args:
+            frame: the total frame of the output array
+            data: directional vector data
+
+        Returns:
+
+            [frame,3] ndarray of euclidean angle
         """
         if frame is None:
-            result = torch.zeros(3)
+            result = np.zeros(3)
             x = data[0]
             y = data[1]
             z = data[2]
-            l = torch.sqrt(torch.add(torch.mul(x,x), torch.mul(y,y)))
-            result[2] = torch.atan2(y, x)
-            result[1] = (torch.add(torch.atan2(z, l),torch.tensor(np.pi/2)))
+            l = np.sqrt(np.add(np.mul(x,x), np.mul(y,y)))
+            result[2] = np.atan2(y, x)
+            result[1] = (np.add(np.atan2(z, l),np.pi/2))
         else:
-            result = torch.zeros((frame, 3))
+            result = np.zeros((frame, 3))
             
             x = data[:, 0]; y = data[:, 1]; z = data[:, 2]
-            l = torch.sqrt(torch.add(torch.mul(x,x), torch.mul(y,y)))
-            result[:, 2] = (torch.add(torch.atan2(y, x),torch.tensor(np.pi/2)))
-            result[:, 0] =  torch.tensor(np.pi/2)# (torch.sub(torch.tensor(np.pi/2),torch.atan2(z, l)))
-            print(result)
+            l = np.sqrt(np.add(np.mul(x,x), np.mul(y,y)))
+            result[:, 2] = (np.add(np.atan2(y, x),np.pi/2))
+            result[:, 0] =  np.pi/2# (torch.sub(torch.tensor(np.pi/2),torch.atan2(z, l)))
+            #print(result)
         
         return result
 
     def cam_motion_linear_motion(self, velocity=10, dir=np.array([1, 0, 0]), tracking=0):
         """
-        velocity: the speed of the camera, unit mm/frame
-        dir: the directional vector of the velocity, should be a unit vector
-        tracking: the parameter determine whether the camera will point at the center defined in __init__
-        the camera doing constant speed motion with a fixed direction
+        generate camera position vector of constant speed linear motion with a fixed direction 
+
+        Args:
+            self: the obj itself
+            velocity: the speed of the camera, unit mm/frame
+            dir: the directional vector of the velocity, should be a unit vector
+            tracking: the parameter determine whether the camera will point at the center defined in __init__
+
+        Returns:
+
+            Null
         """
         dir = dir/np.linalg.norm(dir)
         frames = self.frame
-        self.camera_pos = torch.add(self.camera_pos,torch.mul(torch.tensor(dir*velocity),torch.tensor(np.linspace(0,frames,frames)).reshape(frames,1)));
+        self.camera_pos = np.add(self.camera_pos,np.mul(dir*velocity),np.linspace(0,frames,frames).reshape(frames,1));
         if tracking != 0:
-            dirVec = torch.sub(self.center,self.camera_pos)
+            dirVec = np.sub(self.center,self.camera_pos)
             self.camera_arg = self.get_angle(frames,dirVec)
         else:
-            dirVec = torch.sub(torch.tensor(self.center),self.camera_pos[0])
+            dirVec = np.sub(self.center,self.camera_pos[0])
             self.camera_arg[:,:] = Camera.get_angle(dirVec)
         
         return
 
     def cam_motion_linear_motion2(self, sta_point, end_point, tracking = 0):
         """
-        sta_point & end_point: ndarray(float16) of start and end point
-        tracking: the parameter determine whether the camera will point at the center defined in __init__
-        the camera doing constant speed motion with known start and end point
+        generate camera position vector of constant speed linear motion with known start and end point
+
+        Args:
+            self: the obj itself
+            sta_point & end_point: ndarray(float16) of start and end point
+            tracking: the parameter determine whether the camera will point at the center defined in __init__
+
+        Returns:
+
+            Null
+        
         """
         frames = self.frame
-        self.camera_pos = torch.tensor(np.linspace(sta_point, end_point, frames));
+        self.camera_pos = np.linspace(sta_point, end_point, frames);
         if tracking != 0:
-            dirVec = torch.sub(torch.tensor(self.center),self.camera_pos)
+            dirVec = np.sub(self.center,self.camera_pos)
             self.camera_arg = Camera.get_angle(frames,dirVec)
         else:
-            dirVec = torch.sub(torch.tensor(self.center),self.camera_pos[0])
+            dirVec = np.sub(self.center,self.camera_pos[0])
             self.camera_arg[:,:] = Camera.get_angle(dirVec)
         
         return
 
     def cam_motion_rotation(self, data, radius=3000, angle=np.pi * 2):
         """
-        radius: the radius of rotation
-        angle: the entire arc the carmera travels
-        get 2d rotation around center with given radius and angle (the center is defined in __init__ )
+        generate camera position vector of constant speed rotation around center with given radius and angle
+        * the center is the geometry center of individuals in dataset
+
+        Args:
+            self: the obj itself
+            radius: the radius of rotation
+            angle: the entire arc the carmera travels
+
+        Returns:
+
+            Null
+
         """
         frames = self.frame
 
@@ -291,12 +337,27 @@ class Camera:
         z_2center = np.array([0] * frames, dtype=np.float16).reshape(frames, 1)
         xyz_2center = np.concatenate((np.concatenate((x_2center, y_2center), 1), z_2center), 1)
 
-        self.camera_pos = torch.add(torch.tensor([self.center]*frames).reshape(frames,3), torch.tensor(xyz_2center));
-        self.camera_arg = self.get_angle(frames, torch.sub(self.center,self.camera_pos))
+        self.camera_pos = np.add([self.center]*frames.reshape(frames,3), xyz_2center);
+        self.camera_arg = self.get_angle(frames, self.center,self.camera_pos)
 
         return
 
     def __read_parameter(self,type = "phone"):
+        '''
+        function used for reading intrinsics parameter from files
+
+        Args:
+            self: the obj itself
+            type: camera type
+        
+        Returns:
+
+            Null
+
+        Raises:
+            more config required for different types of camera, eg. monitor, drones
+        '''
+
         con = ConfigParser();
         con.read('configs.ini');
         self.fx = con.getfloat('camera_parameter', 'fx')
@@ -307,20 +368,51 @@ class Camera:
 
         return
 
-    def phone(self, data, pointList = None, motionType = "line"):
-        if pointList is None:
-            start = np.array(self.center) + np.append(np.random.randint(-10000,10000,size = (2)),np.array([1300]));
-            end = np.array(self.center) + np.append(np.random.randint(-10000,10000,size = (2)),np.array([1300]));
-        else:
-            start = pointList[0];
-            end = pointList[1];
+    def phone(self, data, pointList = None, motionType = "line", load = 0):
+        '''
+        This func is used for generating a camera looks like phone view
 
-        if motionType == "line":
-            Camera.cam_motion_linear_motion2(self, sta_point = start, end_point = end, tracking = self.ifTracking);
-        elif motionType == "circle":
+        Args: 
+            self: the object itself
+            data: original 3d data
+            pointList: the specific start and end point, default None
+            motionType: what type of motion the camera should do: default constant speed linear motion
+            load: used to control whether read already generated camera parameter
+
+        Returns:
+
+            Null
+
+        '''
+
+        if load == 0:
             pass
         else:
-            pass
+            if pointList is None:
+                while True:
+                    start = np.array(self.center) + np.append(np.random.randint(-10000,10000,size = (2)),np.array([1300]));
+                    end = np.array(self.center) + np.append(np.random.randint(-10000,10000,size = (2)),np.array([1300]));
+                    
+                    if motionType == "line":
+                        Camera.cam_motion_linear_motion2(self, sta_point = start, end_point = end, tracking = self.ifTracking);
+                    elif motionType == "circle":
+                        pass
+                    else:
+                        pass
+                    
+                    if Camera.collision_check(data, self.camera_pos):
+                        break
+            else:
+                start = pointList[0];
+                end = pointList[1];
+
+                if motionType == "line":
+                    Camera.cam_motion_linear_motion2(self, sta_point = start, end_point = end, tracking = self.ifTracking);
+                elif motionType == "circle":
+                    pass
+                else:
+                    pass
+
 
         Camera.__read_parameter(self,"phone")
         Camera.__exmat_generator(self)
@@ -329,23 +421,87 @@ class Camera:
 
         return
 
+    def collision_check(man,pth):
+        '''
+        the func used to check whether the path of the camera go through one's body
+        default man's width is 200 mm
+
+        Args:
+            man: n,x,17,3 ndarray represent data of human
+            pth: the camera path
+
+        Returns:
+
+            1: if no collision occures
+            0: if collisions occure
+        '''
+        man = man[:,:,10,:].reshape(man.shape[0],man.shape[1],man.shape[3])
+        for i in range(man.shape[0]):
+            if np.min(np.sqrt(np.sum(np.square(man[i] - pth), axis=1))) <= 200:
+                return 0
+        return 1
+
     def update_camera(self):
         """
         Updating the extrinsic matrix
+
+        **this function is have no function until now!!
         """
         Camera.__exmat_generator(self)
+        Camera.__inmat_generator(self)
+
+        pass
+
+    def save_camera(self, number):
+        '''
+        the function used for saving the extrinsics parameter of camera into harddrive, in order for later use
+
+        Args: 
+            self: object itself
+            number: the number of the camera in camera array
+
+        Rerurns:
+
+            Null
+
+        '''
+
+        save_file = np.array([self.camera_pos,self.camera_arg])
+        save_pth = "data\camera_" + str(number) + ".npy"
+        np.save(save_pth,save_file)
+
+        pass
+
+    def __load_camera(self, pth):
+        '''
+        the function used to load extrinsics parameter of a saved camera
+
+        Args:
+            self: the obj itself
+            pth: saved camera file path
+
+        Returns:
+
+            Null
+
+        '''
+
+        tmp = np.load(pth)
+        self.camera_pos = tmp[0]
+        self.camera_arg = tmp[1]
 
         pass
 
     def __visc(self):
         """
         A function with TONS OF BUGS, plz DO NOT use it
+        
         """
 
 
         ax = plt.figure().add_subplot(projection='3d')
-        dir = np.array(torch.matmul(self.R,torch.tensor(np.array([0,0,1000]*self.frame,\
-            dtype = np.float16)).reshape(self.frame,3,1)).reshape(self.frame,3))
+        dir = np.array(np.matmul(self.R,np.array([0,0,1000]*self.frame,\
+            dtype = np.float16).reshape(self.frame,3,1)).reshape(self.frame,3))
         pos = np.array(self.camera_pos)
         x = torch.stack((torch.tensor(pos[:, 0]), torch.tensor(pos[:, 0] + dir[:, 0])), 0)
         y = torch.stack((torch.tensor(pos[:, 1]), torch.tensor(pos[:, 1] + dir[:, 1])), 0)
@@ -364,3 +520,19 @@ class Camera:
 
         plt.show()
         return
+
+
+
+
+
+def dist(vect1,vect2):
+    """
+    used for reference
+    """
+
+    try:
+        return np.sqrt(np.sum(np.square(vect1 - vect2), axis=1))
+
+    except:
+        print("plz input the same dimension")
+
