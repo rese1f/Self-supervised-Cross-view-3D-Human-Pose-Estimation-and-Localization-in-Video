689d32f914bfe38d43a3fd6b376a33cb977cfc4a
Mu Xie <xiem@live.com>
2021-07-17 23:41:31 +0800

    fix bugs, add functions

--- VideoPose3D/visualize.py
+++ VideoPose3D/visualize.py
@@ -18,6 +18,8 @@ class Visualization:
         args = args_4_visc()
         print(args)
         self.ifcomp = args.compare
+        self.ds = args.dataset
+        self.pb = args.playback
         #load data
         
         Visualization.load_camera_view(self, args)
@@ -36,8 +38,15 @@ class Visualization:
 
     def load_camera_view(self, args):
         try:
-            dataset_orig = np.load('output/data_multi_output_' + args.dataset + '.npz', allow_pickle=True)["posetions_2d"].item()
-            self.dataset = dataset_orig[0]
+            dataset_orig = np.load('output/data_multi_output_' + args.dataset + '.npz', allow_pickle=True)["positions_2d"].item()
+            
+
+            self.count_key_list = list(dataset_orig.keys())
+            if args.sample is not None: 
+                self.sample_key = int(args.sample); 
+            else: 
+                self.sample_key = random.choice( self.count_key_list )
+            self.dataset = dataset_orig[self.sample_key]
             
         except KeyError:
             print('Sample does not exist! Please input right sample number')
@@ -48,19 +57,27 @@ class Visualization:
         
         try:
             prediction_orig = np.load('output/data_multi_output_' + args.dataset + '.npz', allow_pickle=True)["positions_3d"].item()
+            self.prediction = prediction_orig[self.count_key_list[self.sample_key]]
             
-            self.count_key_list = list(prediction_orig.keys())
-            if args.sample is not None: 
-                sample_key = int(args.sample); 
-            else: 
-                sample_key = random.choice( self.count_key_list )
+            self.pos_pred = list()
+            for item in self.prediction["pose_pred"]: self.pos_pred.append(item.cpu().detach().numpy())
+            self.pos_pred = np.array(self.pos_pred).squeeze()
 
-            self.prediction = prediction_orig[self.count_key_list[sample_key]]
-            self.pos_pred = self.prediction["pose_pred"][0].transpose(0,1).transpose(0,2)
-            self.pos_pred = torch.mul(self.pos_pred,torch.tensor(1e3))
-            #self.pos_pred = torch.add(self.pos_pred, self.prediction["T"][0])
+            self.pos_trans = list()
+            for item in self.prediction["T"]: self.pos_trans.append(item.cpu().detach().numpy())
+            self.pos_trans = np.array(self.pos_trans).squeeze()
+            
+            self.pos_pred = self.pos_pred.transpose((0,2,1,3)).transpose((1,0,2,3))
+            self.pos_pred = self.pos_pred * 1e3
+            #for i in range(self.pos_pred.shape[0]):
+            #    for j in range(self.pos_pred.shape[1]):
+            #        self.pos_pred[i,j] = self.pos_pred[i,j] + self.pos_trans[i,j]*1e3
+            self.pos_pred = self.pos_pred + self.pos_trans * 1e3
             self.pos_pred[:,:,:,1] = - self.pos_pred[:,:,:,1]
-            self.pos_pred = self.pos_pred.transpose(0,2).transpose(0,1)
+            self.pos_pred = self.pos_pred.transpose((1,0,2,3)).transpose((0,2,1,3))            
+            #print(self.pos_pred.shape)
+            #print(self.pos_pred[1])
+            
         except KeyError:
             print('Sample does not exist! Please input right sample number')
 
@@ -73,7 +90,7 @@ class Visualization:
         try:
             self.view_key_list = list(self.dataset.keys())
             self.n = self.dataset[self.view_key_list[0]]['pose_2d'].shape[0]
-            self.x = self.dataset[self.view_key_list[0]]['pose_2d'].shape[1] - self.prediction["receptive_field"]
+            self.x = self.dataset[self.view_key_list[0]]['pose_2d'].shape[1] - self.prediction["receptive_field"][0] + 1
             self.skeleton = dataset_metadata['skeleton']
             self.joints_num = dataset_metadata['num_joints']
         except KeyError:
@@ -81,8 +98,7 @@ class Visualization:
         self.radius = 4000
         self.radius_m = 4000
         self.center_c = Visualization.__get_center(self.dataset, self.view_key_list)
-        self.center_p = torch.mean(self.pos_pred[:,0,10,:],axis=0).cpu().detach().numpy()
-        print(self.center_p)
+        self.center_p = np.mean(self.pos_pred[:,0,0,:],axis=0)
 
         pass
 
@@ -109,14 +125,14 @@ class Visualization:
 
         pass
         
-    def plt2D(self, view_key, frame):
-        self.ax[view_key][1].clear()
-        multiperson_data = self.dataset[view_key]["pose_2d"] * 1e3; k = 0
+    def plt2D(self, ax, data, camdata, frame):
+        ax.clear()
+        multiperson_data = data; k = 0
         for person in multiperson_data:
             for i in self.skeleton:
                 x = np.stack((person[frame, i[0], 0], person[frame, i[1], 0]), 0)
                 y = np.stack((person[frame, i[0], 1], person[frame, i[1], 1]), 0)
-                self.ax[view_key][1].plot(x, y, lw=2, c=self.color[k],alpha=0.6); 
+                ax.plot(x, y, lw=2, c=self.color[k],alpha=0.6); 
             k += 1
             
             for j in range(17):
@@ -125,83 +141,72 @@ class Visualization:
                 c = person[frame,j,2]
 
                 if c == 1:
-                    self.ax[view_key][1].plot(x, y,'.',color='g',alpha=1)
+                    ax.plot(x, y,'.',color='g',alpha=1)
 
                 if c == -1:
-                    self.ax[view_key][1].plot(x,y,'.',color="r",alpha=1)
+                    ax.plot(x,y,'.',color="r",alpha=1)
 
-        camdata = self.dataset[view_key]['camera']            
         fx = camdata[2]/2
         fy = camdata[3]/2
         cx = camdata[0]
         cy = camdata[1]    
-        self.ax[view_key][1].set_xlim([cx-fx,cx+fx])
-        self.ax[view_key][1].set_ylim([cy-fy,cy+fy])
+        ax.set_xlim([cx-fx,cx+fx])
+        ax.set_ylim([cy-fy,cy+fy])
         pass
 
 
-    def plt3D(self, view_key, frame):
-        self.ax[view_key][0].clear()
-        multiperson_data = self.dataset[view_key]["pose_c"] * 1e3; k = 0
+    def plt3D(self, ax, data, center, frame, dot = False, arrow = True, clear = True, ifscale = True, ifroot = True):
+        if clear: ax.clear()
+        multiperson_data = data; k = 0
         for person in multiperson_data:
+            if k != 1: continue
             for i in self.skeleton:
                 x = np.stack((person[frame, i[0], 0], person[frame, i[1], 0]), 0)
                 z = np.stack((person[frame, i[0], 1], person[frame, i[1], 1]), 0)
                 y = np.stack((person[frame, i[0], 2], person[frame, i[1], 2]), 0)
-                self.ax[view_key][0].plot3D(x, y, z, lw=2, c=self.color[k], alpha = 0.8); 
+                ax.plot3D(x, y, z, lw=2, c=self.color[k], alpha = 0.8); 
             k+=1
+            if ifroot:
+                root = person[frame,0,:]
+                ax.plot3D(root[0], root[2], root[1], '.',color="r",alpha=1)
+                label = '(%d, %d, %d)' % (root[0], root[2], root[1])
+                ax.text(root[0], root[2], root[1], label)
+            
 
-        self.ax[view_key][0].arrow3D(0,0,0,
-           0,1,0,
-           mutation_scale=20,
-           arrowstyle="-|>",
-           linestyle='dashed')
+            if dot:
+                for j in range(17):
+                    x = person[frame,j,0]
+                    y = person[frame,j,1]
+                    z = person[frame,j,2]
 
+                    if j != 0: ax.plot3D(x, y, z, '.',color="r",alpha=1)
 
-        self.ax[view_key][0].set_xlim3d([self.center_c[view_key][0] - 2*self.radius, self.center_c[view_key][0] + 2*self.radius]) # 画布大小
-        self.ax[view_key][0].set_ylim3d([self.center_c[view_key][2] - 2*self.radius, self.center_c[view_key][2] + 2*self.radius])
-        self.ax[view_key][0].set_zlim3d([-self.radius/2 + self.center_c[view_key][1], self.radius/2 + self.center_c[view_key][1]])
+        if arrow:
+            ax.arrow3D(0,0,0,
+            0,1,0,
+            mutation_scale=20,
+            arrowstyle="-|>",
+            linestyle='dashed')
+
+        if ifscale:
+            ax.set_xlim3d([center[0] - 2*self.radius, center[0] + 2*self.radius]) # 画布大小
+            ax.set_ylim3d([center[2] - 2*self.radius, center[2] + 2*self.radius])
+            ax.set_zlim3d([-self.radius/2 + center[1], self.radius/2 + center[1]])
         pass
 
-    def plotPred(self, frame):
-        if self.ifcomp:
-            prediction_data = self.pos_pred; k = 0
-            for person in prediction_data:
-                for i in self.skeleton:
-                    x = torch.stack((person[frame, i[0], 0], person[frame, i[1], 0]), 0).cpu().detach().numpy()
-                    z = torch.stack((person[frame, i[0], 1], person[frame, i[1], 1]), 0).cpu().detach().numpy()
-                    y = torch.stack((person[frame, i[0], 2], person[frame, i[1], 2]), 0).cpu().detach().numpy()
-                    self.ax[self.view_key_list[0]][0].plot3D(x, y, z, lw=2, c=self.color[-1], alpha = 0.8); 
-                k+=1
 
-        else:
-            self.bx.clear()
-            prediction_data = self.pos_pred; k = 0
-            for person in prediction_data:
-                for i in self.skeleton:
-                    x = torch.stack((person[frame, i[0], 0], person[frame, i[1], 0]), 0).cpu().detach().numpy()
-                    z = torch.stack((person[frame, i[0], 1], person[frame, i[1], 1]), 0).cpu().detach().numpy()
-                    y = torch.stack((person[frame, i[0], 2], person[frame, i[1], 2]), 0).cpu().detach().numpy()
-                    self.bx.plot3D(x, y, z, lw=2, c=self.color[k], alpha = 0.8); 
-                k+=1
-
-            self.bx.set_xlim3d([self.center_p[0] - 2*self.radius_m, self.center_p[0] + 2*self.radius_m]) # 画布大小
-            self.bx.set_ylim3d([self.center_p[2] - 2*self.radius_m, self.center_p[2] + 2*self.radius_m])
-            self.bx.set_zlim3d([-self.radius_m/2 + self.center_p[1], self.radius_m/2 + self.center_p[1]])
-
-        pass
 
         
     def updater(self, frame):
 
         if self.ifcomp:
             for key in self.view_key_list:
-                Visualization.plt2D(self, key, frame)
-                Visualization.plt3D(self, key, frame)
+                Visualization.plt2D(self, self.ax[key][1], self.dataset[key]["pose_2d"], self.dataset[key]['camera'], frame)
+                Visualization.plt3D(self, self.ax[key][0], self.dataset[key]["pose_c"] * 1e3, self.center_c[key], frame, False, True)
                 pass
-            Visualization.plotPred(self, frame)
+            Visualization.plt3D(self, self.ax[self.view_key_list[0]][0] ,self.pos_pred, self.center_p, frame, False, False, False, False)
         else:
-            Visualization.plotPred(self, frame)
+            Visualization.plt3D(self, self.bx, self.pos_pred, self.center_p, frame, True, True)
 
         if __name__ == '__main__':
             self.pbar.update(1)
@@ -215,7 +220,8 @@ class Visualization:
         '''
         anim = FuncAnimation(self.fig, self.updater, self.x, interval=1)
         plt.show()
-        #anim.save(self.save_name, writer='pillow', fps=165)
+        #if self.pb: anim.save("output/data_multi_output_" + self.ds + ".gif", writer='imagemagick')
+        if self.pb:anim.save("output/data_multi_output_" + self.ds + ".gif", writer='pillow', fps=165)
 
         return
 
@@ -225,6 +231,21 @@ class Visualization:
             center_dict[key] = np.mean(data[key]["pose_c"][:,0,10,:],axis=0)*1e3
             pass
         return center_dict
+
+    def check_data(self, n):
+        try:
+            data = self.pos_pred[n]
+            for eachframe in data:
+                try:
+                    input("Press enter to continue")
+                    print(eachframe)
+                except SyntaxError:
+                    pass
+                except KeyboardInterrupt:
+                    break
+        except IndexError:
+            print("index out of bound")
+        pass
 
 #   this prt used for arrow drawing
 class Arrow3D(FancyArrowPatch):
@@ -263,5 +284,7 @@ if __name__ == '__main__':
     
     filename = '1'
     v = Visualization()
+    #v.check_data(1)
     v.animate()
+
 
