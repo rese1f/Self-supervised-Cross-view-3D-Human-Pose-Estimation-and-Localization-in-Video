40942e506347ac55d71e7f414d7804f30b0aac0a
Reself <846074811@qq.com>
2021-09-12 10:18:34 +0800

    update

--- DataGenerators/utils/cam_traj.py
+++ DataGenerators/utils/cam_traj.py
@@ -9,8 +9,12 @@ def parse_args():
                         help="mean height, unit: mm")
     parser.add_argument('--d', default=2000, type=int,
                         help="mean distance, unit: mm")
-    parser.add_argument('--v', default=20, type=int,
+    parser.add_argument('--v', default=10, type=int,
                         help="mean velocity, unit: mm per frame")
+    parser.add_argument('--alpha', default=0.01, type=float,
+                        help="close coefficient")
+    parser.add_argument('--beta', default=0.9, type=float,
+                        help="momentum coefficient")
     
     args = parser.parse_args()
     return args
@@ -19,26 +23,40 @@ def cam_traj(pose_3d):
     """this function is to generate a reasonable camera trajction
 
     Args:
-        pose_3d[array](n,f,3)
+        pose_3d[array]: (n,f,3)
     
     Returns:
-        camera trajction[array](f,3)
+        camera trajction[array]: (f,3)
     """
     args = parse_args()
     print(args)
     pose_2d = pose_3d.transpose(1,0,2)[...,:2]
+    f = pose_2d.shape[0]
     o, r = bounding_circle(pose_2d)
-    # init cam
+    # init cam position and velocity
+    p = np.empty((f,2))
+    v = np.empty((f,2))
+    h = random(args.h)
+    angle = np.random.rand()*2*np.pi
+    clockwise = np.random.choice([-1,1])
+    d = random(args.d)
+    p[0] = o[0] + (r[0] + d)*np.array([np.sin(angle), np.cos(angle)])
+    v[0] = np.zeros((2))
+    for t in range(1,f):
+        v[t] = (1-args.beta)*(args.alpha*d*radial_component(p,o,t-1) + random(args.v)*clockwise*tangential_component(p,o,t-1)) + args.beta*v[t-1]
+        p[t] = p[t-1] + v[t]
+    cam_traj = np.insert(p,2,values=h,axis=1)
+    return cam_traj
     
 def bounding_circle(pose_2d):
     """this function is to generate a reasonable camera trajction
 
     Args:
-        pose_2d (array)[f,n,2]
+        pose_2d (array): [f,n,2]
     
     Returns:
-        o (array)[f,[center]]
-        r (array){f,[radius]]
+        o (array): [f,[center]]
+        r (array): [f,[radius]]
     """
     x = pose_2d[...,0]
     y = pose_2d[...,1]
@@ -49,6 +67,39 @@ def bounding_circle(pose_2d):
     o = np.vstack(((xmin+xmax)/2,(ymin+ymax)/2)).transpose(1,0)
     r = np.sqrt((xmax-xmin)**2+(ymax-xmin)**2)
     return o, r
+
+def radial_component(p,o,t):
+    """this function returns the radial unit component at time t
+
+    Args:
+        p ([array]): [2]
+        o ([array]): [2]
+        t ([int]): time
+    
+    Returns:
+        v_r ([array]): [2]
+    """
+    
+    v_r = p[t] - o[t]
+    v_r /= np.linalg.norm(v_r)
+    return v_r
+
+def tangential_component(p,o,t):
+    """this function returns the tangential unit component at time t
+
+    Args:
+        p ([array]): [2]
+        o ([array]): [2]
+        t ([int]): time
+    
+    Returns:
+        v_t ([array]): [2]
+    """
+    rot90 = np.array([[ 0,1],
+                      [-1,0]])
+    v_t = np.matmul(p[t] - o[t], rot90)
+    v_t /= np.linalg.norm(v_t)
+    return v_t
 
 def random(x:float)-> float:
     """random number with mean x
@@ -64,13 +115,12 @@ def random(x:float)-> float:
 
 if __name__=='__main__':
     # 2-people 3-frames
-    pose_3d = np.array([
-        [[1,0,0],
-         [1,1,1],
-         [2,0,1],],
-        [[2,0,2],
-         [3,0,1],
-         [2,1,1],],
-    ])
-    cam_traj(pose_3d)
+    pose_3d = np.zeros((3,1000,3))
+    p = cam_traj(pose_3d)
+    import matplotlib.pyplot as plt
+    plt.plot(p[:,0],p[:,1])
+    plt.scatter([0], [0], s=25, c='r')
+    plt.xlim(-3000,3000)
+    plt.ylim(-3000,3000)
+    plt.show()
     
