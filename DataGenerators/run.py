5dda6ad82557b3e1ed8c600efc02f32f710f66a9
Reself <846074811@qq.com>
2021-09-15 17:36:05 +0800

    update

--- DataGenerators/run.py
+++ DataGenerators/run.py
@@ -19,16 +19,16 @@ print(args)
 
 print('Loading data...')
 
-dataset = np.load('./raw_data/data_3d_' + args.dataset + '.npz', allow_pickle=True)['positions_3d'].item()
+dataset = np.load('../raw_data/data_3d_' + args.dataset + '.npz', allow_pickle=True)['positions_3d'].item()
 dict_keys = dataset.keys()
 
-if not os.path.exists('./data'):
+if not os.path.exists('../data'):
     print('Creating output path...')
-    os.makedirs('./data')
+    os.makedirs('../data')
 else:
     print('Cleaning output path...')
-    shutil.rmtree('./data')
-    os.makedirs('./data')
+    shutil.rmtree('../data')
+    os.makedirs('../data')
 
 
 print('Loading camera...')
@@ -104,6 +104,6 @@ dataset{
         }
     }
 '''
-np.savez_compressed('./data/'+output_filename, dataset=dataset_zip)
+np.savez_compressed('../data/'+output_filename, dataset=dataset_zip)
 
 print('Done.')
