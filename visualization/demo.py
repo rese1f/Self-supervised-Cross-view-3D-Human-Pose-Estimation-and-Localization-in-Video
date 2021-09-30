162c4412034d855d0d74a7c95c1dcb10518f487d
biechi <haob.19@intl.zju.edu.cn>
2021-09-30 14:50:23 +0800

    reformatted demo.py

--- visualization/demo.py
+++ visualization/demo.py
@@ -1,4 +1,3 @@
-  
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2020 Max-Planck-Gesellschaft zur Förderung der Wissenschaften e.V. (MPG),
@@ -27,29 +26,27 @@ import os
 import torch
 
 betas = np.array([
-            np.array([ 2.25176191, -3.7883464, 0.46747496, 3.89178988,
-                      2.20098416, 0.26102114, -3.07428093, 0.55708514,
-                      -3.94442258, -2.88552087])])
-num_betas=10
-batch_size=1
-star = STAR(gender='female',num_betas=num_betas)
+    np.array([2.25176191, -3.7883464, 0.46747496, 3.89178988,
+              2.20098416, 0.26102114, -3.07428093, 0.55708514,
+              -3.94442258, -2.88552087])])
+num_betas = 10
+batch_size = 1
+star = STAR(gender='female', num_betas=num_betas)
 
 # Zero pose
 # TODO: edit this part into poses
-poses = torch.cuda.FloatTensor(np.zeros((batch_size,72)))
-
-
+poses = torch.cuda.FloatTensor(np.zeros((batch_size, 72)))
 
 betas = torch.cuda.FloatTensor(betas)
-trans = torch.cuda.FloatTensor(np.zeros((batch_size,3)))
-model = star.forward(poses, betas,trans).cpu().detach().numpy()
+trans = torch.cuda.FloatTensor(np.zeros((batch_size, 3)))
+model = star.forward(poses, betas, trans).cpu().detach().numpy()
 # shaped = model.v_shaped[-1, :, :]
 
 outmesh_path = './hello_star.obj'
 with open(outmesh_path, 'w') as fp:
     for i in model:
         for v in i:
-            fp.write( 'v %f %f %f\n' % ( v[0], v[1], v[2]) )
-    for f in star.f+1:
-        fp.write( 'f %d %d %d\n' %  (f[0], f[1], f[2]) )
+            fp.write('v %f %f %f\n' % (v[0], v[1], v[2]))
+    for f in star.f + 1:
+        fp.write('f %d %d %d\n' % (f[0], f[1], f[2]))
 print('done.')
